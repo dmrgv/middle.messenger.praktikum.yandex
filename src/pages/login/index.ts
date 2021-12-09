3b44a5d5c4edc53fe5a82b76d3962001ac3963ef
LAPTOP-Q04NR3KL\dmrgv <dmtrrgv@gmail.com>
2021-12-10 02:20:09 +0300

    submit form added

--- src/pages/login/index.ts
+++ src/pages/login/index.ts
@@ -13,12 +13,40 @@ export default class Login extends Block {
   }
 
   componentDidMount() {
+    this._element?.querySelector('#login-sign-up')?.addEventListener('click', () => {
+      window.location.href = './register'
+    })
+
     const form = this._element?.getElementsByTagName('form')[0] as HTMLFormElement
 
     this.props.children.forEach((child: object) => {
       const input = new Input(child).getContent()
       const target = form.querySelector('.inputs-block')
       if (target) target.appendChild(input)
+    })
+
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      let error = false
+
+      const newChildren = this.props.children.map((childData: object) => {
+        const elem = this._element?.querySelector(`#${childData.inputId}`) as HTMLInputElement
+        if (!childData.validator.test(elem.value)) {
+          error = true
+          return {
+            ...childData,
+            error: elem.value ? childData.errorText : 'Поле не должно быть пустым',
+            inputErrorClassName: 'error',
+          }
+        }
+        return childData
+      })
+
+      if (error) {
+        this.setProps({ children: newChildren })
+      } else {
+        window.location.href = './chats.html'
+      }
     })
   }
 }
--- src/pages/login/login-template.ts
+++ src/pages/login/login-template.ts
@@ -1,16 +1,15 @@
 const loginTemplate = `<div class='form-wrapper login-wrapper'>
   <div class='card-block'>
     <h2>Вход</h2>
-    <form autocomplete="off">
+    <form id="login-form" autocomplete="off">
         <div class='inputs-block'>
           {{#each inputs}}
             {{{input}}}
           {{/each}}
         </div>
-        <div class='buttons-block'>
-          <!-- Здесь будет кнопка submit отправки формы (пока переход) -->     
-          <label><a id='login-sign-in' href='./chats.html' class='button'>Авторизоваться</a></label>
-          <label><a id='login-sign-up' href='./register.html' class='link'>Нет аккаунта?</a></label>
+        <div class='buttons-block'>     
+          <button type='submit' class='button' form='login-form'>Авторизоваться</button>
+          {{{button}}}
         </div>
     </form>
   </div>
